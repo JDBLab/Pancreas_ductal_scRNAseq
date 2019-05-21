1e44979fa1c7e19bbf8a19fc20318dbad3290c90
Fahd Qadir <fahdqadir@gmail.com>
2019-05-21 17:07:22 -0400

    Update 1_environment_setup.R
    Errors in plotly installation corrected

--- R_analysis_by_experiment/1_environment_setup.R
+++ R_analysis_by_experiment/1_environment_setup.R
@@ -23,7 +23,7 @@ install.packages("cowplot")
 install.packages('ggrepel')
 install.packages("R.utils")
 install.packages("gridExtra")
-install.packages("plot_ly")
+install.packages("plotly")
 
 # Install the scRNAseq analysis package Seurat
 install.packages('Seurat') #This installs Seurat v3.0.0 as of 5/20/2019, if Seurat is updated direct installation to v3.0.0
@@ -46,7 +46,7 @@ library(ggrepel)
 library(dplyr)
 library(Seurat)
 library(monocle)
-library(plot_ly)
+library(plotly)
 
 # CONFIRM CORRECT INSTALL ####
 # Confirm package version of Seurat and Monocle
