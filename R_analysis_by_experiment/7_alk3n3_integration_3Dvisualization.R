220c0749a19535fcd0717eb0d2e661261bae3d40
Fahd Qadir <fahdqadir@gmail.com>
2019-05-22 12:47:48 -0400

    Update 7_alk3n3_integration_3Dvisualization.R
    Code correction for pre-processing

--- R_analysis_by_experiment/7_alk3n3_integration_3Dvisualization.R
+++ R_analysis_by_experiment/7_alk3n3_integration_3Dvisualization.R
@@ -22,21 +22,41 @@ library(ggrepel)
 library(dplyr)
 library(Seurat)
 library(monocle)
-library(plot_ly)
+library(plotly)
 
 # CONFIRM CORRECT INSTALL ####
 # Confirm package version of Seurat and Monocle
 packageVersion("Seurat")
 packageVersion("monocle")
+packageVersion("plotly")
 
-# INTERACTIVE 3D tSNE PLOTING ####
-# Plotting all cells in 3D, having the same color combinations for each celll as in the 2D tSNE analysis
-plotting.data <- FetchData(object = yourseuratobject, vars = c("tSNE_1", "tSNE_2", "tSNE_3", "seurat_clusters"))
-plotting.data$label <- paste(rownames(plotting.data))
+# 3D PLOTTING ####
+# Construct a dataframe using data from your pre-clustered Seurat v3.0.0 object
+# Here 'seurat_clusters' is list of numeric cluster identities, you can find it here: yourseuratobject[["seurat_cluster"]], 
+# or yourseuratobject$seurat_clusters, where 'yourseuratobject' is a Seurat object created with Seurat v3.0.0
+yourseuratobject <- ThisIsWhateverYourSeuratObjectIs
+
+# Re-run tSNE s that you have accurate calculations for all tSNE(s)
+yourseuratobject <- RunTSNE(yourseuratobject,
+                        reduction.use = "pca",
+                        dims.use = 1:20,
+                        dim.embed = 3)
+
+# Extract tSNE information from Seurat Object
+tsne_1 <- yourseuratobject[["tsne"]]@cell.embeddings[,1]
+tsne_2 <- yourseuratobject[["tsne"]]@cell.embeddings[,2]
+tsne_3 <- yourseuratobject[["tsne"]]@cell.embeddings[,3]
+
+# Prepare a dataframe for cell plotting
+plot.data <- FetchData(object = yourseuratobject, vars = c("tSNE_1", "tSNE_2", "tSNE_3", "seurat_clusters"))
+
+# Make a column of row name identities (these will be your cell/barcode names)
+plot.data$label <- paste(rownames(plot.data))
+
+# Plot your data, in this example my Seurat object had 21 clusters (0-20)
 plot_ly(data = plotting.data, 
         x = ~tSNE_1, y = ~tSNE_2, z = ~tSNE_3, 
         color = ~seurat_clusters, 
-        opacity = .5,
         colors = c("lightseagreen",
                    "gray50",
                    "darkgreen",
@@ -60,27 +80,39 @@ plot_ly(data = plotting.data,
                    "darkmagenta"),
         type = "scatter3d", 
         mode = "markers", 
-        marker = list(size = 4, width=2), 
-        text=~label,
-        hoverinfo="text")
+        marker = list(size = 5, width=2), # controls size of points
+        text=~label, #This is that extra column we made earlier for which we will use
+        hoverinfo="text") #When you visualize your plotly object, hovering your mouse pointer over a point shows cell names
 
-# Looking at gene expression for a particular gene and plotting that in 3D
-plotting.data <- FetchData(object = yourseuratobject, vars = c("tSNE_1", "tSNE_2", "tSNE_3", "CTNND1"))
-plotting.data$changed <- ifelse(test = plotting.data$CTNND1 <1, yes = plotting.data$CTNND1, no = 1)
-plotting.data$label <- paste(rownames(plotting.data)," - ", plotting.data$CTNND1, sep="")
+# Say you wanto make a gene-expression 3D plot, where you can plot gene expression against a color scale
+# Here using the same seurat object as above, we extract gene expression information for beta-actin 'ACTB'
+# create a dataframe
+plotting.data <- FetchData(object = yourseuratobject, vars = c("tSNE_1", "tSNE_2", "tSNE_3", "ACTB"))
 
+# Say you want change the scale, so that every cell having an expression >1 will be one color
+# Basically, you are re-adjusting the scale here, so that any cell having a certain expression 
+# will light up on your 3D plot
+
+# First make another column in your dataframe, where all values above 1 are re-assigned a value of 1
+# This information is stored in the 'changed' column of your dataframe
+plotting.data$changed <- ifelse(test = plotting.data$KRT19 <1, yes = plotting.data$KRT19, no = 1)
+
+# Add the label column, so that now the column has 'cellname-its expression value'
+plotting.data$label <- paste(rownames(plotting.data)," - ", plotting.data$ACTB, sep="")
+
+# Plot your data, in this example my Seurat object had 21 clusters (0-20), and cells express a gene called ACTB
 plot_ly(data = plotting.data, 
         x = ~tSNE_1, y = ~tSNE_2, z = ~tSNE_3, 
-        color = ~changed,
+        color = ~changed, # you can just run this against the column for the gene as well using ~ACTB
         opacity = .5,
         colors = c('darkgreen', 'red'), 
         type = "scatter3d", 
         mode = "markers",
-        marker = list(size = 4, width=2), 
+        marker = list(size = 5, width=2), 
         text=~label,
         hoverinfo="text"
 )
 
-################## #
-################## #
-# CODE END ####
+# Thank you for reading and using this code to further your scRNAseq analysis!
+# If you liked it, dont forget to acknowledge, fork and star!
+# Have a wonderful day!!
